84c8071c2acdb01fac240b7eb538d83e6eb28d9b
Moddy Pikiso <moddypikiso17@gmail.com>
2021-10-20 00:18:00 +0200

    correcting tests for travis build

--- test/registration.test.js
+++ test/registration.test.js
@@ -43,36 +43,37 @@ describe('The basic database registration app', function () {
 
     it('should be able display registration numbers for Paarl', async function () {
 
-        await regDatabase.pushRegister("CA 231-234");
-        await regDatabase.pushRegister("CL 231-234");
-        await regDatabase.pushRegister("CJ 231-234");
-        await regDatabase.pushRegister("CA 231-234");
+        await regDatabase.pushRegister("CA 231-234", 1);
+        await regDatabase.pushRegister("CL 231-234", 2);
+        await regDatabase.pushRegister("CJ 231-234", 3);
+        await regDatabase.pushRegister("CA 231-234", 1);
 
-        var data = await regDatabase.gettingID("CL")
+        var data = await regDatabase.gettingID("CL", 2)
         assert.equal(1, data.length);
 
     });
 
     it('should be able display registration numbers Stellenbosch', async function () {
 
-        await regDatabase.pushRegister("CA 231-234");
-        await regDatabase.pushRegister("CL 231-234");
-        await regDatabase.pushRegister("CJ 231-234");
-        await regDatabase.pushRegister("CA 231-234");
+        await regDatabase.pushRegister("CA 231-234", 1);
+        await regDatabase.pushRegister("CL 231-234", 2);
+        await regDatabase.pushRegister("CJ 231-234", 3);
+        await regDatabase.pushRegister("CA 231-234", 1);
 
-        var data = await regDatabase.gettingID("CJ")
+        var data = await regDatabase.gettingID("CJ", 3)
         assert.equal(1, data.length);
 
     });
 
     it('should be able display registration numbers Cape Town', async function () {
 
-        await regDatabase.pushRegister("CA 231-234");
-        await regDatabase.pushRegister("CL 231-234");
-        await regDatabase.pushRegister("CJ 231-234");
-        await regDatabase.pushRegister("CA 231-236");
+      
+        await regDatabase.pushRegister("CA 231-234", 1);
+        await regDatabase.pushRegister("CL 231-234", 2);
+        await regDatabase.pushRegister("CJ 231-234", 3);
+        await regDatabase.pushRegister("CA 231-234", 1);
 
-        var data = await regDatabase.gettingID("CA")
+        var data = await regDatabase.gettingID("CA", 1)
         assert.equal(1, data.length);
 
     });
@@ -81,22 +82,25 @@ describe('The basic database registration app', function () {
 
     it('should not duplicate registration numbers input', async function () {
 
-        await regDatabase.pushRegister("CA 231-234");
-        await regDatabase.pushRegister("Ca 231-234");
-        await regDatabase.pushRegister("ca 231-234");
-        await regDatabase.pushRegister("CA 231-234");
+       
+        await regDatabase.pushRegister("CA 231-234", 1);
+        await regDatabase.pushRegister("CL 231-234", 2);
+        await regDatabase.pushRegister("CJ 231-234", 3);
+        await regDatabase.pushRegister("CA 231-234", 1);
 
         var data = await regDatabase.getRegs()
-        assert.equal(1, data.length);
+        assert.equal(3, data.length);
 
     });
 
     it('should clear data in database', async function () {
 
         // input into database
-        await regDatabase.pushRegister("CA 123-123");
-        await regDatabase.pushRegister("CA 123-123");
-        await regDatabase.pushRegister("CA 123-123");
+       
+        await regDatabase.pushRegister("CA 231-234", 1);
+        await regDatabase.pushRegister("CL 231-234", 2);
+        await regDatabase.pushRegister("CJ 231-234", 3);
+        await regDatabase.pushRegister("CA 231-234", 1);
 
         // clearing my database
         await regDatabase.resetting();
@@ -112,12 +116,12 @@ describe('The basic database registration app', function () {
 
     it('should return a registration number using the database', async function () {
 
-        await regDatabase.pushRegister("CA 123-123");
+        await regDatabase.pushRegister("CA 123-123", 1);
 
         var data = "CA 123-123"
         
 
-        let categories = await regDatabase.showRegs()
+        let categories = await regDatabase.showRegs(1)
         var msg = categories.rows
 
         assert.deepEqual(data.rows, msg);
