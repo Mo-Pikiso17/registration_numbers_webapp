7b984053dc4f72f4f8dcbda6c07f8e280148cdb6
Moddy Pikiso <moddypikiso17@gmail.com>
2021-10-21 15:37:41 +0200

    Insert into tables.sql

--- tables.sql
+++ tables.sql
@@ -2,3 +2,7 @@ CREATE TABLE towns(townID BIGSERIAL PRIMARY KEY NOT NULL, town_name text NOT NUL
 
 
 CREATE TABLE registration_numbers(regID BIGSERIAL PRIMARY KEY NOT NULL, regNumber text NOT NULL, parentID INT, FOREIGN KEY(parentID) REFERENCES towns(townID));
+
+INSERT INTO towns(town_name, regCode)VALUES ('Cape Town', 'CA');
+INSERT INTO towns(town_name, regCode)VALUES ('Paarl', 'CL');
+INSERT INTO towns(town_name, regCode)VALUES ('Stellenbosch', 'CJ');
